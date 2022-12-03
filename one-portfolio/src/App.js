b3b3585678d30a42e5ed06404edbc80f6c797b6d
Luis Sanchez <luisanchezdh@gmail.com>
2022-12-03 11:39:51 -0500

    navbar finished

--- one-portfolio/src/App.js
+++ one-portfolio/src/App.js
@@ -1,6 +1,13 @@
+import Navbar from "./components/Navbar";
+
+
+
+
 function App() {
   return (
-    <h1 className="text-4xl font-bold font-signature">Hello</h1>
+    <div>
+      <Navbar />
+    </div>
   );
 }
 
