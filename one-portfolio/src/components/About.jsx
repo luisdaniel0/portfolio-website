b7a97495ae5d1cf15fc600c130e5c401f628315d
Luis Sanchez <luisanchezdh@gmail.com>
2022-12-05 09:23:27 -0500

    fixed selfie

--- one-portfolio/src/components/About.jsx
+++ one-portfolio/src/components/About.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <div>
-        <p>About Me</p>
-      </div>
-      <p>Junior Developer with a passion for coding. In my previous role, I had the opportunity to manage a large team of people with a common objective to deliver quality products to clients. As someone who's always worked in a fast paced environment, problem solving quickly with an optimistic demeanor has always been one of my best attributes and I'm excited to bring this quality and more to my next role.
+    <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
 
-      </p>
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">About Me</p>
+        </div>
+        <p className="text-xl mt-20">Junior Developer with a passion for coding. In my previous role, I had the opportunity to manage a large team of people with a common objective to deliver quality products to clients. As someone who's always worked in a fast paced environment, problem solving quickly with an optimistic demeanor has always been one of my best attributes and I'm excited to bring this quality and more to my next role.
+
+        </p>
+      </div>
     </div>
   )
 }
